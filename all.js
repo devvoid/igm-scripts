8c594309031bd97d5f2729fe719a283a01cd1da0
Void <33298798+devvoid@users.noreply.github.com>
2022-09-19 07:24:46 -0700

    Create all.js

---
+++ all.js
@@ -0,0 +1,3 @@
+javascript:(function(){with(document)(head.appendChild(createElement('script')).src='https://cdn.jsdelivr.net/gh/devvoid/igm-scripts@61ce0a6eb4d1f6264c39c2fcf24818abf958728f/beautify.js')._})();
+javascript:(function(){with(document)(head.appendChild(createElement('script')).src='https://cdn.jsdelivr.net/gh/devvoid/igm-scripts@61ce0a6eb4d1f6264c39c2fcf24818abf958728f/autoclicker.js')._})();
+javascript:(function(){with(document)(head.appendChild(createElement('script')).src='https://cdn.jsdelivr.net/gh/devvoid/igm-scripts@61ce0a6eb4d1f6264c39c2fcf24818abf958728f/multibuy.js')._})();
