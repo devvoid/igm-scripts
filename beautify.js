61ce0a6eb4d1f6264c39c2fcf24818abf958728f
Void <33298798+devvoid@users.noreply.github.com>
2022-09-19 07:19:12 -0700

    Update beautify.js

--- beautify.js
+++ beautify.js
@@ -1,23 +1,57 @@
-nums = ['k','M','B','T','Qa','Qi', 'Sx', 'Sp', 'Oc', 'No', 'De', 'UnD', 'DuD', 'TrD', 'QaD', 'QiD', 'SeD', 'SpD', 'OcD', 'NoD', 'Vi', 'UnV'];
+nums = [
+    ' thousand',
+    ' million',
+    ' billion',
+    ' trillion',
+    ' quadrillion',
+    ' quintillion',
+    ' sextillion',
+    ' septillion',
+    ' octillion',
+    ' nonillion',
+    ' decillion',
+    ' undecillion',
+    ' duodecillion',
+    ' tredecillion',
+    ' quattuordecillion ',
+    ' quindecillion',
+    ' sexdecillion',
+    ' septdecillion',
+    ' octodecillion',
+    ' nonillion',
+    ' vigintillion',
+    ' unvigintillion'
+];
 
 
 function Beautify(num,floats)
 {
-if (!isFinite(num)) return 'Infinity'
-if(num < 1e3 || num >= parseFloat('1e' + 3*(nums.length))) return Math.round(num)
-var i = 0;
-while(num >= 1000)
-{
-num/=1000;
-i++;
-}
-num = Math.round(num*1000)/1000;
-if(num>=1000)
-{
-num/=1000;
-i++;
-num = Math.round(num*1000)/1000;
-} //deals with rounding errors
-num = num.toFixed(3);
-return num + nums[i - 1]
+    if (!isFinite(num)) {
+        return 'Infinity'
+    }
+
+    if(num < 1e3 || num >= parseFloat('1e' + 3*(nums.length))) {
+        return Math.round(num)
+    }
+
+    var i = 0;
+    while(num >= 1000)
+    {
+        num/=1000;
+        i++;
+    }
+
+    // Limits the number to three decimal points
+    num = Math.round(num*1000)/1000;
+    
+    // This deals with rounding errors
+    if(num>=1000) {
+        num/=1000;
+        i++;
+        num = Math.round(num*1000)/1000;
+    }
+    
+    num = num.toFixed(3);
+    
+    return num + nums[i - 1]
 }
